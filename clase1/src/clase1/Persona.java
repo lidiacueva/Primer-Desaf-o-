8b861d3d10c64e719f2723166406826f5b27cff4
Lidia Cueva <lidiamanuelaceciliac@gmail.com>
2025-02-17 11:43:50 -0500

    Segundo desafio

--- clase1/src/clase1/Persona.java
+++ clase1/src/clase1/Persona.java
@@ -1,5 +1,20 @@
 package clase1;
 
 public class Persona {
-
+ public static void main(String[] args) {
+	 persona t1 = new persona(1,"Maria Estefan");
+     persona t2 = new persona(2,"Fernando Chavez")	 ;
+     persona t3 = new persona(3,"Elena Pinto");
+     persona t4 = new persona(4,"Karl Canto");
+     persona t5 = new persona(5,"Elsa Limo");
+List<Personas> lista new ArrayList<Persona>();
+	 Lista.add(t1);
+	 Lista.add(t2);
+	 Lista.add(t3);
+	 Lista.add(t4);
+	 Lista.add(t5);
+	 For (Persona t :lista){
+		 System.out.printl(t.toString());
+	 }
+ }
 }
